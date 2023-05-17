a3e6f2f58064fab58d989097515c3fd24000ef63
joav <jim.acero.valcarcel@gmail.com>
2023-05-17 17:01:39 -0500

    Show next Pokemon one by one Reveal Turn

---
+++ src/components/pokemons/pokemons-image.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable qwik/jsx-img */
+import { component$, useSignal, useTask$ } from "@builder.io/qwik"
+
+interface Props {
+  id: number;
+  size?: number;
+  backImage?: boolean;
+  isVisible?: boolean;
+}
+
+export const PokemonImage = component$(({ id, size = 200, backImage, isVisible }: Props) => {
+  const imageLoaded = useSignal(false);
+
+  useTask$(({ track }) => {
+    track(() => id);
+
+    imageLoaded.value = false;
+  })
+
+  const imageUrl = (
+    backImage ?
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png` :
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+  );
+
+  return (
+    <div class="flex items-center justify-center" style={{
+      width: `${size}px`,
+      height: `${size}px`
+    }}>
+      {!imageLoaded.value && <span>Cargando...</span>}
+      <img
+        src={imageUrl}
+        alt="Pokemon Sprite"
+        style={{ width: `${size}px` }}
+        onLoad$={() => imageLoaded.value = true}
+        class={[{
+          hidden: !imageLoaded.value,
+          'brightness-0': !isVisible
+        }, 'transition-all']}
+      />
+    </div>
+  )
+})
