8d511d2a56e4eaa62d928113aba29da9ce0e034b
joav <jim.acero.valcarcel@gmail.com>
2023-05-31 17:41:59 -0500

    Pokemon common-layout

---
+++ src/routes/counter/layout.tsx
@@ -0,0 +1,14 @@
+import { Slot, component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div class="flex flex-col justify-center items-center">
+      <Slot />
+
+      <Link href="/" class="mt-10">
+        Regresar
+      </Link>
+    </div>
+  );
+});
